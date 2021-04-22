f3ed5393829e04c882215820475221a4eb02e764
rfarina97 <72233948+rfarina97@users.noreply.github.com>
2021-04-22 19:48:02 -0400

    Update musicTest.py

--- musicTest.py
+++ musicTest.py
@@ -8,25 +8,35 @@ track    = 0
 channel  = 0
 time     = 0   # In beats
 duration = 1   # In beats
-tempo    = 120  # In BPM
+tempo    = 100  # In BPM
 volume   = 100 # 0-127, as per the MIDI standard
 
-##Blues scale
-##3 2 1 1 3 2 por semitonos
-bluesScale = [60, 63, 65, 66, 67, 70, 72]
-bluesScale2 = [3, 2, 1, 1, 3 ,2]
+##Las escalas NO repiren su ultima nota
+##Todas DEBEN sumar 12
+mayorScale = [0, 2, 2, 1, 2, 2, 2, 1]
+bluesScale = [0, 3, 2, 1, 1, 3, 2]
+minorScale = [0, 2, 1, 2, 2, 1, 2, 2]
+pentatonicScale = [0, 2, 2, 3, 2, 3]
 
 randDuration = [0.25, 0.5, 1, 2]
 
 MyMIDI = MIDIFile(1) 
 # One track, defaults to format 1 (tempo track automatically created)
+
 MyMIDI.addTempo(track, time, tempo)
 
+print("Enter base note: ")
+baseNote = int(input())
+
+
+
 for i in range(16):
-	pitch = degrees[random.randint(0,6)]
-	duration = randDuration[random.randint(0,3)]
-	MyMIDI.addNote(track, channel, pitch, time, duration, volume)
-	print ("pitch: ", pitch)
+	#cambiar rango a las notas de la scala
+	noteInScale = degrees[random.randint(0,7)]
+	duration = 1
+	#duration = randDuration[random.randint(0,3)]
+	MyMIDI.addNote(track, channel, baseNote + noteInScale, time, duration, volume)
+	print ("pitch: ", noteInScale + baseNote)
 	print ("time: ", time)
 	time = time + 1
 
